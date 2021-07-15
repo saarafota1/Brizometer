b56b80da3bdce7be6826e2e9966b033adb438a4d
Saar Afuta <saar@APLICATZIA.COM>
2021-07-15 09:56:54 +0300

    Client And Server App

---
+++ brizometer_client/src/components/Brizometer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="brizometer">
+      <TopFiveCities />
+      <SelectedCitiesList  />
+      <SelectableCities @refreshList="refreshList()" />
+  </div>
+</template>
+
+<script>
+import SelectableCities from './SelectableCities.vue'
+import SelectedCitiesList from './SelectedCitiesList.vue'
+import TopFiveCities from './TopFiveCities.vue'
+export default {
+  components: { TopFiveCities, SelectedCitiesList, SelectableCities },
+  name: 'Brizometer',
+  data(){
+    return {
+      selectedUser:{id:1,name:'Saar'},
+      forceRefresh:false,
+    }
+  },
+  props: {
+    msg: String
+  },
+  methods:{
+    refreshList(){
+      this.forceRefresh = !this.forceRefresh;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .brizometer{
+        max-width: 800px;
+        margin: 0 auto;
+        text-align: left;
+    }
+</style>
